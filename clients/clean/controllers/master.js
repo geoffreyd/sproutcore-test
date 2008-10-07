73adf40fec3f06e9818f9d4070de87f3b79d200b
geoffreyd <geoffreyd@gmail.com>
2008-10-07 16:31:28 +1000

    ops, the clients folder didn't get added

---
+++ clients/clean/controllers/master.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Clean.MasterController
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends SC.Object
+  @author AuthorName
+  @version 0.1
+  @static
+*/
+Clean.masterController = SC.Object.create(
+/** @scope Clean.masterController */ {
+
+  sideBarCollapsed: NO,
+
+}) ;
